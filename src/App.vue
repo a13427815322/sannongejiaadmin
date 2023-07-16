17cf8f4f0773819707dce124e7031cf3c662cad0
xgz <xgz@sise.com>
2023-07-16 16:42:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h1>我是app根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = '我爱你中国'
+export { str }
+</script>
+
+<style scoped lang=" scss">
+.box{
+    width: 600px;
+    height: 400px;
+     background-color: black;
+    h1{
+        color:white
+    }
+}
+</style>
